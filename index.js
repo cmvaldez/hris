743b3b56eb105bf46ceb4682ad8268b5c736c380
Cheyenne Valdez <cmvaldez@student.apc.edu.ph>
2024-09-06 01:53:10 +0800

    Put basic routing endpoints.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/hrisRoutes.js'
 
 const app = express();
 const PORT = 3000; //or 4000
+
+routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Node and express server is running on port ${PORT}`)
